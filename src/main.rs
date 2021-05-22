efb8c0fb2fc14ad3a7a7feddefbd0dee1adb370f
etoal83 <etoal83@gmail.com>
2021-05-23 01:21:17 +0900

    Derive StructOpt to Cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    println!("pattern: {}, path: {:?}", args.pattern, args.path.into_os_string());
 }
