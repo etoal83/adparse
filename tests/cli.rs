138766175fd2d354a4515eca258c6ffc25b82bf4
etoal83 <etoal83@gmail.com>
2021-05-23 23:07:59 +0900

    Add happy path case in integration tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,7 +1,9 @@
+use anyhow::Result;
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
+use std::io::{self, Write};
 use std::process::Command;
-use anyhow::Result;
+use tempfile::NamedTempFile;
 
 #[test]
 fn file_doesnt_exist() -> Result<()> {
@@ -11,6 +13,20 @@ fn file_doesnt_exist() -> Result<()> {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("No such file or directory"));
+    
+    Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<()> {
+    let mut file = NamedTempFile::new()?;
+    writeln!(file, "A test\nFirst content\nSecond content\nEnd of file")?;
+
+    let mut cmd = Command::cargo_bin("adparse")?;
+    cmd.arg("content").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("First content\nSecond content"));
     
     Ok(())
 }
